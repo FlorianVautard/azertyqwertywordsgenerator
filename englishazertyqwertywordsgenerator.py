951e5f0036e0926013e4669f6e521e590722a6da
FlorianVautard <contact@florianvautard.fr>
2024-01-04 12:15:59 +0100

    Allow to generate group of words

--- englishazertyqwertywordsgenerator.py
+++ englishazertyqwertywordsgenerator.py
@@ -1,31 +1,39 @@
 #!/bin/python
-import re
-import sys
-import random
-# pip3 install requests
 import requests
+import random
+import string
 
-def get_words():
-    url = "https://raw.githubusercontent.com/dwyl/english-words/master/words.txt"
+def obtenir_liste_de_mots(url):
     response = requests.get(url)
-    words = response.text.split("\n")
-    return words
+    if response.status_code == 200:
+        return response.text.split('\n')
+    else:
+        print(f"Échec de récupération de la liste de mots. Code d'état : {response.status_code}")
+        return []
 
-def filter_words(words):
-    pattern = re.compile(r'^[ertyuiopsdfghjkl]+$')
-    filtered_words = [word for word in words if pattern.match(word)]
-    return filtered_words
+def est_mot_valide(mot):
+    return all(c.isalpha() or c.isspace() for c in mot)
 
-def display_words_in_batches(words, batch_size=20):
-    random.shuffle(words)  # Mélanger les mots
-    for i in range(0, len(words), batch_size):
-        batch = words[i:i+batch_size]
-        print("\n".join(batch))
-        input("\nAppuyez sur Entrée pour afficher les 20 mots suivants...")
+def afficher_groupes_de_mots(mots, taille_groupe_defaut, nombre_groupes):
+    taille_groupe = max(taille_groupe_defaut, 1)
+    mots_valides = [mot for mot in mots if est_mot_valide(mot)]
+
+    for i in range(nombre_groupes):
+        groupe = random.sample(mots_valides, taille_groupe)
+        print("".join(groupe))
 
 if __name__ == "__main__":
-    words = get_words()
-    filtered_words = filter_words(words)
+    url_liste_mots = "https://raw.githubusercontent.com/dwyl/english-words/master/words.txt"
+    mots = obtenir_liste_de_mots(url_liste_mots)
 
-    print("Mots contenant uniquement les lettres 'ertyuiopsdfghjkl' (mélangés):")
-    display_words_in_batches(filtered_words)
+    try:
+        taille_groupe_defaut = int(input("Entrez le nombre de mots par groupe (par défaut est 4) : ") or 4)
+        nombre_groupes = 20
+    except ValueError:
+        print("Entrée invalide. Utilisation de la taille de groupe par défaut, qui est 4.")
+        taille_groupe_defaut = 4
+        nombre_groupes = 20
+
+    print(f"\nAffichage de {nombre_groupes} groupes de mots, chaque groupe contenant {taille_groupe_defaut} mots sans espace entre eux, sans caractères spéciaux :")
+    afficher_groupes_de_mots(mots, taille_groupe_defaut, nombre_groupes)
+
